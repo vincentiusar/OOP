75a7ca896934322b9ead02ccc7a7e6f51e5b5dcc
vincentiusar <vincentiusarnoldfridolin@gmail.com>
2021-10-14 14:36:52 +0700

    create package

---
+++ Project management/src/subProject/subProject.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in subProject Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package subProject;
+
+/**
+ *
+ * @author Vincentius
+ */
+public class subProject {
+    
+}
