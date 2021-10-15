c6ea275453cef738559e0febaf3cb805c1e1baa5
Naufalyozha <59946871+Naufalyozha@users.noreply.github.com>
2021-10-15 11:31:46 +0700

    Update subProject.java

--- Project management/src/subProject/subProject.java
+++ Project management/src/subProject/subProject.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in subProject Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package subProject;
 
-/**
- *
- * @author Vincentius
- */
+import Orang.Orang;
+import Project.Project;
+import.java.util.Scanner;
+
 public class subProject {
+    private String id_subProj;
+    private String nama;
+    private String who;
+    private Boolean done;
     
+    public subProject (String id_subProj, String nama, String who, Boolean done){
+        this.setId(id_subProj);
+        this.setNama(nama);
+        this.setWho(who);
+        this.CheckDone(done);
+    }
+    public void setId(String id_subProj){
+        this.Id_subProj = Id_subproj;   
+    }
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    public void setWho(String who){
+        this.who = who;    
+    }
+    public boolean CheckDone( boolean done){
+        boolean found = false;
+     
+        return found;
+    }
 }
